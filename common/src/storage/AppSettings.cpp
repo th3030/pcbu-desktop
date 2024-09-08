d2d3de88130e6cce4c1f2c16610889ea9d733449
th3030 <info@jaymohabier.nl>
2024-09-08 13:11:39 +0200

    Change default values

--- common/src/storage/AppSettings.cpp
+++ common/src/storage/AppSettings.cpp
@@ -38,8 +38,8 @@ PCBUAppStorage AppSettings::Get() {
         def.pairingServerPort = 43295;
         def.unlockServerPort = 43296;
         def.clientSocketTimeout = 120;
-        def.clientConnectTimeout = 5;
-        def.clientConnectRetries = 2;
+        def.clientConnectTimeout = 2;
+        def.clientConnectRetries = 30;
         def.waitForKeyPress = false;
         def.isManualUnlockEnabled = false;
         spdlog::info("Creating new app storage...");
