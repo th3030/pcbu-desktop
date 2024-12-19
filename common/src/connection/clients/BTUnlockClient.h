5c8635dc961bfeba65e72768248a976bb1285df5
th3030 <19645039+th3030@users.noreply.github.com>
2024-12-19 12:04:13 +0100

    Add delayed connection booleans. Hold the second server and release at successfull prompt or after timeout. In Windows restart BT Service if connection takes too long to establish

--- common/src/connection/clients/BTUnlockClient.h
+++ common/src/connection/clients/BTUnlockClient.h
@@ -8,7 +8,7 @@
 
 class BTUnlockClient : public BaseUnlockConnection {
 public:
-    BTUnlockClient(const std::string& deviceAddress, const PairedDevice& device);
+    BTUnlockClient(const std::string& deviceAddress, const PairedDevice& device, const bool &secondServer);
 
     bool Start() override;
     void Stop() override;
@@ -19,6 +19,11 @@ private:
     int m_Channel;
     SOCKET m_ClientSocket;
     std::string m_DeviceAddress;
+    static bool isAlreadyConnected;
+#ifdef WINDOWS
+    static bool restartPending;
+    static int countedConnection;
+#endif
 };
 
 #endif
